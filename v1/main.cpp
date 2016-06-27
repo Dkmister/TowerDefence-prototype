740fcd05de00dc394e10f91be0c45e838d12e640
João Batista <Joo Batista>
2016-06-26 21:15:18 -0300

    Comentário teste.

--- v1/main.cpp
+++ v1/main.cpp
@@ -6,7 +6,7 @@
 
 const GLint g_width = 900;
 const GLint g_height = 600;
-
+//vilmar é uma bichona
 static void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
       glfwSetWindowShouldClose(window, GL_TRUE);
